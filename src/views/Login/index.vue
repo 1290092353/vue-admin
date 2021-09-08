9881de213ec6eb2d605f8ae5ce6ceb7a0201b9a9
1290092353 <1290092353@qq.com>
2021-09-08 11:04:43 +0800

    xaio

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        这里是登录页
+    </div>
+</template>
+<script>
+export default {
+    name:'login'
+}
+</script>
+<style lang="scss" scoped>
+.aaa {
+    height: 100vh;
+    background-color: #344a5f;
+}    
+</style>
